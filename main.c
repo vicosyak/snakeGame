75b45a8865b8ed5974ccecdc77f69cfec62874dc
vicosyak <kryvshenkovika0@gmail.com>
2026-09-09 14:37:27 +0300

    feat: add input handling and game logic mechanics
    - implement input() with non-blocking key reads using _kbhit and _getch
    - implement logic() for head/tail movement, fruit spawning, and collisions

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
+#include <conio.h>
 
 int gameBoard[15][15];
 int headX, headY;
 int fruitX, fruitY;
 int tailX[100], tailY[100], tailLength;
 bool gameOver;
+char dir;
 
 void setup() {
     srand(time(NULL));
@@ -40,4 +42,67 @@ char boardView() {
     }
 }
 
+void input() {
+    if (_kbhit()) {
+        char key = _getch();
 
+        if (key == 'w') {
+            dir = 'w';
+        }
+        if (key == 's') {
+            dir = 's';
+        }
+        if (key == 'a') {
+            d ir = 'a';
+        }
+        if (key == 'd') {
+            dir = 'd';
+        }
+    }
+}
+
+void logic() {
+    // Рух голови
+    if (dir == 'w') {
+        headY--;
+    } else if (dir == 's') {
+        headY++;
+    } else if (dir == 'a') {
+        headX--;
+    } else if (dir == 'd') {
+        headX++;
+    }
+
+    // Поїдання фрукту
+    if (headX == fruitX && headY == fruitY) {
+        tailLength++;
+        fruitX = rand() % 13;
+        fruitY = rand() % 13;
+    }
+
+    // Рух хвоста
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+
+    tailX[0] = headX;
+    tailY[0] = headY;
+
+    for (int i = 0; i < tailLength; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    if (headX == 0 || headY == 0 || headX == 14 || headY == 14) {
+        gameOver = true;
+    }
+    for (int i = 0; i < tailLength; i++) {
+        if (headX == tailX[i] && headY == tailY[i]) {
+            gameOver = true;
+        }
+    }
+}
